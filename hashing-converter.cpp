eb6700f9bc6003184fead2e7f03e892289153409
Izfaha <rasberry006@gmail.com>
2024-12-22 11:46:49 +0700

    constant-time test for equality

--- hashing-converter.cpp
+++ hashing-converter.cpp
@@ -17,13 +17,10 @@ struct hashing{
 
 int main(){
     if(sodium_init() < 0) {
-        
+        return 1;
     } // Initialize libsodium
 
-    long hash_converter;
-    struct hashing h_converter;
-    cout << "The hash can be letter, number, or special cahracter.";
-    cout << "Place the text here : "; cin >> h_converter.textConverter;
-    hash_converter = sodium_base642bin(h_converter.textConverter);
-    cout << "Your hashing text is : " << h_converter.hashConverter << endl;
+    // constant-time for equality
+    int sodium_memcmp(const void * const b1_, const void * const b2_, size_t len);
+
 }
