7cac471d7d327f8be19b68edd951e97c2234b9db
Izfaha <rasberry006@gmail.com>
2025-02-24 15:52:21 +0700

    day two

--- lib-hash.c
+++ lib-hash.c
@@ -2,15 +2,20 @@
 #include <sodium/crypto_generichash.h>
 #include <sodium/crypto_hash_sha256.h>
 #include <stdio.h>
+#include <string.h>
 
 // Global variable
 
 unsigned char hash[crypto_generichash_BYTES];
-unsigned char hex[crypto_hash_sha256_BYTES]; 
+unsigned char sha256[crypto_hash_sha256_BYTES]; 
+unsigned char sha512[crypto_hash_sha512_BYTES];
 
 // Declare built-in function
 
 void headerScript();
+void hashHexadecimal(const char *input[]);
+
+// Main function 
 
 int main(){
 
@@ -27,13 +32,22 @@ int main(){
     return 0;
 }
 
-// Library for main function
+//  built-in function
 
 void headerScript() {
-    printf("Cryptography Project");
+    printf("=============================================================================\n");
+    printf("Hashing Script Using LibSodium in C Language\n");
+    printf("=============================================================================\n");
 }
 
-void hashHexadecimal(char inputMsg[]){
+void hashHexadecimal(const char *input[]){
+    printf("Input your message you need to hash : "); scanf(input);
 
+    /* size_t = an unsigned integer type used to represent the size of object in bytes 
+        strlen() = calculate the lenght of a given string*/
+    size_t input_len = strlen(input); 
+    
+    // 
+    crypto_generichash(input, sizeof(input), (const unsigned char *)input, input_len, NULL, 0); 
 }
 
